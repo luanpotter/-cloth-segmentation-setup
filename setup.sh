f2aab3fa03643f93d0979b6365aa982477bd3bac
Luan Nico <luanpotter27@gmail.com>
2023-01-24 13:48:18 -0500

    Better setup

--- setup.sh
+++ setup.sh
@@ -6,7 +6,19 @@ function setup_instance {
   sudo apt-get -y install python3-pip libgl1-mesa-glx wget
 }
 
+function setup_cuda {
+  wget https://developer.download.nvidia.com/compute/cuda/11.1.0/local_installers/cuda-repo-debian10-11-1-local_11.1.0-455.23.05-1_amd64.deb
+  sudo dpkg -i cuda-repo-debian10-11-1-local_11.1.0-455.23.05-1_amd64.deb
+  
+  sudo apt-get intall -y software-properties-common # adds add-apt-repository
+  sudo add-apt-repository contrib
+  
+  sudo apt-get update
+  sudo apt-get -y install cuda
+}
+
 function setup_repo {
+  rm -rf cloth-segmentation 2> /dev/null
   git clone https://github.com/levindabhi/cloth-segmentation.git
 
   cp requirements.txt cloth-segmentation/
@@ -31,8 +43,9 @@ function download_input {
   cd ..
 }
 
-rm -rf cloth-segmentation 2> /dev/null
 setup_instance
+setup_cuda
 setup_repo
 download_model
 download_input
+
